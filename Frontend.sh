24c7b98486db0c2399d8cd7a313fa410cad9469b
Santosh <santoshkumar.kollati@gmail.com>
2024-10-29 14:06:47 -0600

    updated header error and added instance creation script

--- Frontend.sh
+++ Frontend.sh
@@ -1,18 +1,18 @@
-echo -e "\e[33>>>>>>>Install Nginx<<<<<<<<\e[0m"
+echo -e "\e[33m>>>>>>Install Nginx<<<<<<<<\e[0m"
 dnf install nginx -y &>>/tmp/frontend-roboshop.log
 
-echo -e "\e[33>>>>>>Removing Web server content<<<<<<<\e[0m"
+echo -e "\e[33m>>>>>>>removing Web server content<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/frontend-roboshop.log
 
-echo -e "\e[33>>>>>>Downloading Roboshop webserver content<<<<<<<\e[0m"
+echo -e "\e[33m>>>>>>Downloading Roboshop webserver content<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/frontend-roboshop.log
 
-echo -e "\e[33>>>>>>Unzipping webserver content <<<<<<<\e[0m"
+echo -e "\e[33m>>>>>>Unzipping webserver content <<<<<<<\e[0m"
 cd /usr/share/nginx/html &>>/tmp/frontend-roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/frontend-roboshop.log
 
 #need to copy config file
 
-echo -e "\e[34m>>>>>>>>>>>>>>>>>Restarting Nginx Service<<<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[33m>>>>>>>>>>>>>>>>>Restarting Nginx Service<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable nginx &>>/tmp/frontend-roboshop.log
 systemctl restart nginx &>>/tmp/frontend-roboshop.log
