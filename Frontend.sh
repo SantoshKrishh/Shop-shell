19e6afe7f9786a6289e61a978418d9dca597d781
Santosh <santoshkumar.kollati@gmail.com>
2024-10-29 13:30:10 -0600

    echo heading and error logging added

--- Frontend.sh
+++ Frontend.sh
@@ -1,13 +1,18 @@
-dnf install nginx -y
+echo -e "\e[33>>>>>>>Install Nginx<<<<<<<<\e[0m"
+dnf install nginx -y &>>/tmp/frontend-roboshop.log
 
-systemctl enable nginx
+echo -e "\e[33>>>>>>Removing Web server content<<<<<<<\e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/frontend-roboshop.log
 
+echo -e "\e[33>>>>>>Downloading Roboshop webserver content<<<<<<<\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/frontend-roboshop.log
 
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[33>>>>>>Unzipping webserver content <<<<<<<\e[0m"
+cd /usr/share/nginx/html &>>/tmp/frontend-roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/frontend-roboshop.log
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+#need to copy config file
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
-systemctl restart nginx
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Restarting Nginx Service<<<<<<<<<<<<<<<<<<<\e[0m"
+systemctl enable nginx &>>/tmp/frontend-roboshop.log
+systemctl restart nginx &>>/tmp/frontend-roboshop.log
