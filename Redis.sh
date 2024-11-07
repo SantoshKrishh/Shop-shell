b5ae29a4562a4ba2594d0400a10a07f227171624
Santosh <santoshkumar.kollati@gmail.com>
2024-11-07 03:36:37 -0700

    corrected sed command

--- Redis.sh
+++ Redis.sh
@@ -8,7 +8,7 @@ echo -e "\e[33m>>>>> Installing Redis <<<<<\e[0m"
 dnf install redis -y &>>/tmp/redis-roboshop.log
 
 echo -e "\e[33m>>>>> Updating Redis listen address <<<<<\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/redis-roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/redis-roboshop.log
 
 echo -e "\e[33m>>>>> Starting Redis service <<<<<\e[0m"
 systemctl enable redis &>>/tmp/redis-roboshop.log
