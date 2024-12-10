7c0aa358a51b7a4544bb4952a3b102b0feb37aab
Santosh <santoshkumar.kollati@gmail.com>
2024-12-10 04:51:22 -0700

    added variables

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,53 +1,58 @@
-#echo -e "\e[33m>>>>><<<<<\e[0m"
+#echo -e "/e[33m >>>>><<<<<  ${nocolor} "
 
-echo -e "\e[33m>>>>> Copying service file <<<<<\e[0m"
-cp /home/centos/Shop-shell/catalogue.service /etc/systemd/system/catalogue.service
+#variables
+component=catalogue
+color="\e[33m"
+nocolor=" ${nocolor} "
 
-echo -e "\e[33m>>>>> Enabling latest NOdejs <<<<<\e[0m"
-dnf module disable nodejs -y &>>/tmp/catalogue-roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Copying service file <<<<< ${nocolor}"
+cp /home/centos/Shop-shell/$component.service /etc/systemd/system/$component.service
 
-echo -e "\e[33m>>>>> Installing NodeJS <<<<<\e[0m"
-dnf install nodejs -y &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Enabling latest NOdejs <<<<< ${nocolor}"
+dnf module disable nodejs -y &>>/tmp/$component-roboshop.log
+dnf module enable nodejs:18 -y &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Create Application user <<<<<\e[0m"
-useradd roboshop &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Installing NodeJS <<<<< ${nocolor}"
+dnf install nodejs -y &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Creating App directory <<<<<\e[0m"
-rm -rf /app &>>/tmp/catalogue-roboshop.log
-mkdir /app &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Create Application user <<<<< ${nocolor}"
+useradd roboshop &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Downloading Application Code <<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Creating App directory <<<<< ${nocolor}"
+rm -rf /app &>>/tmp/$component-roboshop.log
+mkdir /app &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Changing Directory and Extracting Code content <<<<<\e[0m"
-cd /app &>>/tmp/catalogue-roboshop.log
-unzip -o /tmp/catalogue.zip  &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Downloading Application Code <<<<< ${nocolor}"
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Downloading Dependencies <<<<<\e[0m"
-cd /app &>>/tmp/catalogue-roboshop.log
-npm install &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Changing Directory and Extracting Code content <<<<< ${nocolor}"
+cd /app &>>/tmp/$component-roboshop.log
+unzip -o /tmp/$component.zip  &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Replacing <MONGODB-SERVER-IPADDRESS> with IP address  <<<<<\e[0m"
-sed -i -e "s/<MONGODB-SERVER-IPADDRESS>/mongodb.roboshopsk.shop/" /etc/systemd/system/catalogue.service &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Downloading Dependencies <<<<< ${nocolor}"
+cd /app &>>/tmp/$component-roboshop.log
+npm install &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Loading the service <<<<<\e[0m"
-systemctl daemon-reload &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Replacing <MONGODB-SERVER-IPADDRESS> with IP address  <<<<< ${nocolor}"
+sed -i -e "s/<MONGODB-SERVER-IPADDRESS>/mongodb.roboshopsk.shop/" /etc/systemd/system/$component.service &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Starting the service <<<<<\e[0m"
-systemctl enable catalogue &>>/tmp/catalogue-roboshop.log
-systemctl start catalogue &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Loading the service <<<<< ${nocolor}"
+systemctl daemon-reload &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> copying mongodb repo file <<<<<\e[0m"
+echo -e "${color}  >>>>> Starting the service <<<<< ${nocolor}"
+systemctl enable $component &>>/tmp/$component-roboshop.log
+systemctl start $component &>>/tmp/$component-roboshop.log
+
+echo -e "${color}  >>>>> copying mongodb repo file <<<<< ${nocolor}"
 cp /home/centos/Shop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/mongodb-roboshop.log
 
-echo -e "\e[33m>>>>> Installing Mongodb-client <<<<<\e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Installing Mongodb-client <<<<< ${nocolor}"
+dnf install mongodb-org-shell -y &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Loading master data  <<<<<\e[0m"
-mongo --host mongodb.roboshopsk.shop </app/schema/catalogue.js &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Loading master data  <<<<< ${nocolor}"
+mongo --host mongodb.roboshopsk.shop </app/schema/$component.js &>>/tmp/$component-roboshop.log
 
-echo -e "\e[33m>>>>> Restarting the service <<<<<\e[0m"
-systemctl restart catalogue &>>/tmp/catalogue-roboshop.log
+echo -e "${color}  >>>>> Restarting the service <<<<< ${nocolor}"
+systemctl restart $component &>>/tmp/$component-roboshop.log
 
 
